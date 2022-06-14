b330b606ceaa7418c2f9444d43ce24f323a74314
Vera <vera@DESKTOP-QPRLS4I.localdomain>
2022-06-14 17:37:46 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+    const submit = document.querySelector('form');
+    submit.addEventListener("submit", (e) => {
+      e.preventDefault()
+      DoList()
+      submit.reset()
+    
+  });
+  
+  function DoList(){
+    const input = document.getElementById("new-task-description");
+    const Task = document.getElement('li');
+    const deleteButton = document.createElement('button');
+    Task.textContent = `${input,value}`;
+    document.getElementById('task').appendChild(Task);
+  
+  
+    deleteButton.textContent= "DONE";
+    Task.appendChild(deleteButton);
+    deleteButton.addEventListener('click', () => Task.remove())
+  }
+  
+  
+  });
